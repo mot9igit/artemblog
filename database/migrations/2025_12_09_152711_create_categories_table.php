ba13ca7109ff0072103a3d0dbab3055bf546b5c3
mot9igit <artpetropavlovskij@gmail.com>
2025-12-09 21:51:43 +0500

    pre model

---
+++ database/migrations/2025_12_09_152711_create_categories_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('longtitle')->nullable();
+            $table->text('introtext')->nullable();
+            $table->text('content');
+            $table->longText('dev_content')->nullable();
+            $table->string('seotitle')->nullable();
+            $table->text('seodescription')->nullable();
+            $table->boolean('published')->default(false);
+            $table->string('slug', 2048)->unique();
+            $table->string('image')->nullable();
+            $table->datetime('createdon');
+            $table->datetime('publishedon');
+            $table->integer('parent')->unsigned()->nullable();
+            $table->foreign('parent')->references('id')->on('categories')->onDelete('cascade');
+            $table->integer('createdby')->unsigned()->default(0);
+            $table->foreign('createdby')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('updatedby')->unsigned()->default(0);
+            $table->foreign('updatedby')->references('id')->on('users')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
